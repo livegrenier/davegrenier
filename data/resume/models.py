3801ab23d4c2d54e3f07fc62a312218a4eed8554
dave <livegrenier@gmail.com>
2023-10-17 17:26:11 -0400

    commit

--- data/resume/models.py
+++ data/resume/models.py
@@ -10,9 +10,6 @@ class photo(models.Model):
 class about(models.Model):
     title = models.CharField(default='', max_length=200)
     content = RichTextField(default='')
-    subtitle = models.CharField(default='', max_length=200)
-    pdf = models.FileField(default='', upload_to='site_media') 
-    pdf_icon = models.FileField(default='', upload_to='site_media') 
     def __str__(self):
         return self.title
 
@@ -21,6 +18,9 @@ class intro(models.Model):
     text = models.TextField(default='')
     content = RichTextField(default='')
     email = models.CharField(default='', max_length=200)
+    pdf = models.FileField(default='', upload_to='site_media') 
+    pdf_icon = models.FileField(default='', upload_to='site_media') 
+    subtitle = models.CharField(default='', max_length=200)
     def __str__(self):
         return self.title
 
